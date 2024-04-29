608df09b837a45a6509336562e1649a4bfadca89
junglistkirss <45033753+junglistkirss@users.noreply.github.com>
2024-04-29 09:56:44 +0200

    Update AutoAcceptorAttribute.cs
    Remove Toucan namespace name reference

--- Condor.Visitor.Generator.Abstractions/AutoAcceptorAttribute.cs
+++ Condor.Visitor.Generator.Abstractions/AutoAcceptorAttribute.cs
@@ -23,7 +23,7 @@ namespace Condor.Visitor.Generator.Abstractions
     [AttributeUsage(AttributeTargets.Interface, AllowMultiple = true, Inherited = false)]
     public class AutoAcceptorAttribute<T> : Attribute
     {
-        public string AssemblyPart { get; set; } = nameof(Toucan);
+        public string AssemblyPart { get; set; } = default!;
         public bool AllowAbstract { get; set; } = false;
         public bool AllowGeneric { get; set; } = false;
         public bool AllowRecord { get; set; } = true;
